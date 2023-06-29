1350a2d4bc91aab85cac474010c0d8923ca6f7d3
Joao Mateus <joao.amaral@ifam.edu.br>
2023-06-29 12:25:12 -0300

    feat: add cors and set middlewares on express.config

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import { router } from "../routes/index";
+import { userRoutes } from "../routes/auth-routes"
+import morgan from "morgan";
+import cors from "cors";
 
-const createServer = ():express.Application => {
+const createServer = (): express.Application => {
     const app = express();
     app.use(express.urlencoded({ extended: true }))
     app.use(express.json());
-    app.use(router);
-    app.get("/", (_req, res) => {
-        res.send("Hello World")
-    })
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: '*'
+    }));
 
+    app.use("/api/v1", router);
+    app.use('/account/auth', userRoutes)
+    
     return app;
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import { AddressInfo } from "net";
 import { createServer } from "./config/express";
 import http from "http";
-import { userRoutes } from "./routes/auth-routes"
+
 
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
-    const app = await createServer();
-    app.use('/account/auth', userRoutes)
+    const app = createServer();
+    
     const server = http.createServer(app).listen({ port }, () => {
         const adressInfo = server.address() as AddressInfo;
         console.log(`🚀 Server is running on http://localhost:${adressInfo.port}`);
